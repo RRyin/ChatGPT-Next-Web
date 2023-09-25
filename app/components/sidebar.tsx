f158ab4b44fa23e48ebeaba3bb38bbdace9b9357
RRyin <110253089+RRyin@users.noreply.github.com>
2023-09-26 00:11:12 +0800

    Update sidebar.tsx

--- app/components/sidebar.tsx
+++ app/components/sidebar.tsx
@@ -148,7 +148,7 @@ export function SideBar(props: { className?: string }) {
           ChatGPT Next
         </div>
         <div className={styles["sidebar-sub-title"]}>
-          Build your own AI assistant.
+          <p style="color: black; font-weight: bold;">GPT桑，没有你我洋仔怎么活啊(>﹏<)</p>
         </div>
         <div className={styles["sidebar-logo"] + " no-dark"}>
           <ChatGptIcon />
